9a24701cad3a61293359cb0d721dd487a4d9c1a7
fillipe-felix <felipesoares_1993@hotmail.com>
2021-05-24 20:48:38 -0300

    criando os métodos HTTP na controller

---
+++ dio_api_catalogo_jogos_dotnet/Controllers/V1/JogosController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dio_api_catalogo_jogos_dotnet.Controllers.V1
+{
+    [Route("api/V1/[controller]")]
+    [ApiController]
+    public class JogosController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<object>>> Obter()
+        {
+            return Ok();
+        }
+        
+        [HttpGet("{idJogo:guid}")]
+        public async Task<ActionResult<object>> Obter(Guid idJogo)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<object>> InserirJogo(object jogo)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{idJogo:guid}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, object jogo)
+        {
+            return Ok();
+        }
+        
+        [HttpPatch("{idJogo:guid}/preco/{preco:double}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, double preco)
+        {
+            return Ok();
+        }
+        
+        [HttpDelete("{idJogo:guid}")]
+        public async Task<ActionResult> ApagarJogo(Guid idJogo)
+        {
+            return Ok();
+        }
+    }
+}

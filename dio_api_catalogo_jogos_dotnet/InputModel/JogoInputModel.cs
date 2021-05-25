21afbadb3273a8564057455166e1fd22a6f52f08
fillipe-felix <felipesoares_1993@hotmail.com>
2021-05-24 21:05:13 -0300

    Criando JogoInputModel para dados de entrada da request e JogoViewModel para a response da requisição

---
+++ dio_api_catalogo_jogos_dotnet/InputModel/JogoInputModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dio_api_catalogo_jogos_dotnet.InputModel
+{
+    public class JogoInputModel
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "O nome do jogo deve conter entre 3 e 100 caracteres")]
+        public string Nome { get; set; }
+        
+        [Required]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "O nome da produtora deve conter entre 1 e 100 caracteres")]
+        public string Produtora { get; set; }
+        
+        [Required]
+        [Range(1, 100, ErrorMessage = "O preço deve ser no minimo 1 real e no maximo 1000 reais")]
+        public double Preco { get; set; }
+    }
+}
---
+++ dio_api_catalogo_jogos_dotnet/ViewModel/JogoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dio_api_catalogo_jogos_dotnet.ViewModel
+{
+    public class JogoViewModel
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Produtora { get; set; }
+        public double Preco { get; set; }
+    }
+}

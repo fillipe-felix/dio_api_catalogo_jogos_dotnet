1c5971ce1c48f550c6fe8a8804cfb339a013e93b
fillipe-felix <felipesoares_1993@hotmail.com>
2021-05-24 21:58:40 -0300

    Criando a repository e a model de Jogo

---
+++ dio_api_catalogo_jogos_dotnet/Entities/Jogo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace dio_api_catalogo_jogos_dotnet.Entities
+{
+    public class Jogo
+    {
+        public Guid Id { get; set; }
+        
+        public string Nome { get; set; }
+        
+        public string Produtora { get; set; }
+        
+        public double Preco { get; set; }
+    }
+}
---
+++ dio_api_catalogo_jogos_dotnet/Repositories/IJogoRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using dio_api_catalogo_jogos_dotnet.Entities;
+
+namespace dio_api_catalogo_jogos_dotnet.Repositories
+{
+    public interface IJogoRepository
+    {
+        Task<List<Jogo>> Obter(int pagina, int quantidade);
+
+        Task<Jogo> Obter(Guid id);
+
+        Task<List<Jogo>> Obter(string nome, string produtora);
+
+        Task Inserir(Jogo jogo);
+
+        Task Atualizar(Jogo jogo);
+
+        Task Remover(Guid id);
+        
+        Task Dispose();
+    }
+}

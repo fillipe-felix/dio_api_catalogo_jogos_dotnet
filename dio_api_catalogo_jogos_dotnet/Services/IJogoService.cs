61dec2373af6e1d0a795f9374c61442145633203
fillipe-felix <felipesoares_1993@hotmail.com>
2021-05-24 21:05:50 -0300

    Criando a service interface IJogoService e os metodos do CRUD

---
+++ dio_api_catalogo_jogos_dotnet/Services/IJogoService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using dio_api_catalogo_jogos_dotnet.InputModel;
+using dio_api_catalogo_jogos_dotnet.ViewModel;
+
+namespace dio_api_catalogo_jogos_dotnet.Services
+{
+    public interface IJogoService
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+
+        Task<JogoViewModel> Obter(Guid id);
+
+        Task<JogoViewModel> Inserir(JogoInputModel jogo);
+
+        Task Atualizar(Guid id, JogoInputModel jogo);
+
+        Task Remover(Guid id);
+    }
+}

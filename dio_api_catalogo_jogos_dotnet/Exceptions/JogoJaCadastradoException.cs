0d002539641a51d02a6466255c1237e5b7d91be0
fillipe-felix <felipesoares_1993@hotmail.com>
2021-05-24 21:58:57 -0300

    Criando as classses de exception

---
+++ dio_api_catalogo_jogos_dotnet/Exceptions/JogoJaCadastradoException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dio_api_catalogo_jogos_dotnet.Exceptions
+{
+    public class JogoJaCadastradoException : Exception
+    {
+        public JogoJaCadastradoException() : base("Este jogo já esta cadastrado")
+        {
+            
+        }
+    }
+}
---
+++ dio_api_catalogo_jogos_dotnet/Exceptions/JogoNaoCadastradoException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dio_api_catalogo_jogos_dotnet.Exceptions
+{
+    public class JogoNaoCadastradoException : Exception
+    {
+        public JogoNaoCadastradoException() : base("Esta jogo não esta cadastrado")
+        {
+            
+        }
+    }
+}
